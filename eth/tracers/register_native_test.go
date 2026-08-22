1c36b493dc79c22761b4ae5f86ec8b78fbad0bfc
jzhu-eth <zuriel.blockchain@gmail.com>
2026-08-22 20:49:56 +0800

    delete the unused code

--- eth/tracers/register_native_test.go
+++
@@ -1,6 +0,0 @@
-package tracers_test
-
-// Blank import registers native tracers (callTracer, etc.) so they are available
-// to internal (package tracers) tests. This file must use the external test package
-// to avoid a cyclic dependency: eth/tracers/native imports eth/tracers.
-import _ "github.com/photon-storage/bor/eth/tracers/native"
